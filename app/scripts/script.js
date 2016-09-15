ac581398643498e806c29c7dfded7bc979229b41
DomovoiSasha <domovoi_sasha@mail.ru>
2016-09-16 03:41:54 +0500

    Response 320px, js actions

---
+++ app/scripts/script.js
@@ -0,0 +1,11 @@
+$(function(){
+//  $('.main-menu__btn-burger').hover(function(){
+//    $(this'svg').css({
+//      "fill":"#d22856"
+//    })
+//  });
+  
+  $('.logo').click(function(){
+     alert("Click!");
+  });
+});
--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,1 +1,138 @@
-!function(e,t){"use strict";var n="img/sprite.svg",a=1;if(!t.createElementNS||!t.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect)return!0;var o,r,i="localStorage"in e&&null!==e.localStorage,l=function(){t.body.insertAdjacentHTML("afterbegin",r)},c=function(){t.body?l():t.addEventListener("DOMContentLoaded",l)};if(i&&localStorage.getItem("inlineSVGrev")==a&&(r=localStorage.getItem("inlineSVGdata")))return c(),!0;try{o=new XMLHttpRequest,o.open("GET",n,!0),o.onload=function(){o.status>=200&&o.status<400&&(r=o.responseText,c(),i&&(localStorage.setItem("inlineSVGdata",r),localStorage.setItem("inlineSVGrev",a)))},o.send()}catch(e){}}(window,document);
+$(function(){
+  var burgerBtn = $('.main-menu__btn-burger');
+  var burger = $('#burger');
+  var crossBtn = $('.main-menu__btn-cross');
+  var cross = $('#cross');
+  var menuItem = $('.main-menu__item');
+  var header = $('.header-head');
+  var logo = $('.logo');
+  var review1 = $('.review__item:first-child'); 
+  var review2 = $('.review__item:nth-child(2)');
+  var review3 = $('.review__item:last-child');
+  var reviewLabel1 = $('#rewiew1-label');
+  var reviewLabel2 = $('#rewiew2-label');
+  var reviewLabel3 = $('#rewiew3-label');
+  var priceLabel1 = $('#price1-label');
+  var priceLabel2 = $('#price2-label');
+  var priceLabel3 = $('#price3-label');
+  var price = $('.price-table');
+  
+  
+  $(burgerBtn).bind({
+    mouseenter: function(){
+      $(burger).css({"fill": "#d22856",
+                                  "transition":"1s ease"});
+    },
+    mouseleave: function(){
+      $(burger).css({"fill": "#ffffff",
+                                  "transition":"1s ease"});
+    },
+    click: function(){
+      $(this).css({"display":"none",
+                            "transition":"1s ease"});
+      $(crossBtn).css({"display":"block",
+                                      "transition":"1s ease"});
+      $(menuItem).css({"display":"block",
+                                        "transition":"3s"});
+      $(header).css({"background-color":"#283645",
+                                  "border-bottom":"1px solid  #000",
+                                  "transition":"ease"});
+    }
+  });
+  
+  $(crossBtn).bind({
+    mouseenter: function(){
+      $(cross).css({"fill": "#d22856",
+                              "transition":"ease"});
+    },
+    mouseleave: function(){
+      $(cross).css({"fill": "#ffffff",
+                              "transition":"ease"});
+    },
+    click: function(){
+      $(this).css({"display":"none",
+                            "transition":"ease"});
+      $(burgerBtn).css({"display":"block",
+                                        "transition":"ease"});
+      $(menuItem).css({"display":"none",
+                                        "transition":"3s"});
+      $(header).css({"background-color":"rgba(0,0,0,0.4)",
+                                  "border-bottom":"none",
+                                  "transition":"ease"});
+    }
+  });
+  
+  $(menuItem).bind({
+    mouseenter: function(){
+      $(cross).css({"color": "#d22856",
+                               "transition":"ease"});
+    },
+    mouseleave: function(){
+      $(cross).css({"color": "#ffffff",
+                               "transition":"ease"});
+    },
+    mousedown: function(){
+      $(this).css({"color":"rgba(210,40,86,0.3)",
+                            "transition":"ease"});
+    },
+    mouseup: function(){
+      $(this).css({"color":"#d22856",
+                            "transition":"ease"});
+    }
+  }); 
+
+  $(reviewLabel1).click(function(){
+    if  ($('#review1').prop('checked')) {} else {
+      $(review1).css({"display":"block",
+                                              "transition":"ease"});
+      $(review2).css({"display":"none",
+                                              "transition":"ease"});
+      $(review3).css({"display":"none",
+                                              "transition":"ease"});
+    }
+  });
+  
+  $(reviewLabel2).click(function(){
+    if  ($('#review2').prop('checked')) {} else {
+      $(review1).css({"display":"none",
+                                              "transition":"ease"});
+      $(review2).css({"display":"block",
+                                              "transition":"ease"});
+      $(review3).css({"display":"none",
+                                              "transition":"ease"});
+    }
+  });
+  
+  $(reviewLabel3).click(function(){
+    if  ($('#review3').prop('checked')) {} else {
+      $(review1).css({"display":"none",
+                                              "transition":"ease"});
+      $(review2).css({"display":"none",
+                                              "transition":"ease"});
+      $(review3).css({"display":"block",
+                                              "transition":"ease"});
+    }
+  });
+  
+  $(priceLabel1).click(function(){
+    if  ($('#row1').prop('checked')) {} else {
+      $(price).css({"left":"-82.5vw",
+                               "transition":"0.5s ease"});
+    }
+  });
+  
+  $(priceLabel2).click(function(){
+    if  ($('#row2').prop('checked')) {} else {
+      $(price).css({"left":"-170vw",
+                               "transition":"0.5s ease"});
+    }
+  });
+  
+  $(priceLabel3).click(function(){
+    if  ($('#row3').prop('checked')) {} else {
+      $(price).css({"left":"-257.5vw",
+                               "transition":"0.5s ease"});
+    }
+  });
+  
+});
